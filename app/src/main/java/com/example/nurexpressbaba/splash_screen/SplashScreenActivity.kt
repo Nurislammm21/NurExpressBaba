94392cf9b540d3dda920329a1dddccc57ab20813
Nurislam <gggrrraaa1235@gmail.com>
2024-03-04 18:58:13 +0500

    4th commit adding splash_screen and lottie animation for splash screen

---
+++ app/src/main/java/com/example/nurexpressbaba/splash_screen/SplashScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.example.nurexpressbaba.splash_screen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.example.nurexpressbaba.MainActivity
+import com.example.nurexpressbaba.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var timer : CountDownTimer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        timer = object : CountDownTimer(5000,2500){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
+    }
+}
